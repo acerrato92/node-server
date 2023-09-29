949e97f23ff46b8f98b2fdd0ae1a6148364cff10
acerrato92 <132726954+acerrato92@users.noreply.github.com>
2023-09-29 15:35:40 -0600

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,74 +1,94 @@
 const readline = require('readline');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// Estructura de datos para almacenar las tareas
+const Task = {
+  id: 0,
+  indicator: '',
+  description: '',
+  completed: false,
+};
 
+// Lista de tareas
 const tasks = [];
 
-function printTasks() {
-  console.log('Lista de tareas:');
-  tasks.forEach((task, index) => {
-    console.log(`${index + 1}. [${task.completed ? 'X' : ' '}] - ${task.description}`);
-  });
+// Función para añadir una tarea
+function addTask() {
+  // Solicitamos al usuario los datos de la tarea
+  const indicator = readline.question('Indicador: ');
+  const description = readline.question('Descripción: ');
+
+  // Creamos una nueva tarea
+  const task = new Task();
+  task.id = tasks.length + 1;
+  task.indicator = indicator;
+  task.description = description;
+  task.completed = false;
+
+  // Añadimos la tarea a la lista
+  tasks.push(task);
 }
 
-function addTask(description) {
-  tasks.push({ description, completed: false });
-  console.log('Tarea añadida.');
-  printTasks();
-}
+// Función para eliminar una tarea
+function deleteTask() {
+  // Solicitamos al usuario el ID de la tarea a eliminar
+  const id = parseInt(readline.question('ID de la tarea a eliminar: '));
 
-function removeTask(index) {
-  if (index >= 0 && index < tasks.length) {
-    tasks.splice(index, 1);
-    console.log('Tarea eliminada.');
-    printTasks();
-  } else {
-    console.log('Índice de tarea inválido.');
+  // Buscamos la tarea en la lista
+  const task = tasks.find(t => t.id === id);
+
+  // Eliminamos la tarea
+  if (task) {
+    tasks.splice(tasks.indexOf(task), 1);
   }
 }
 
-function completeTask(index) {
-  if (index >= 0 && index < tasks.length) {
-    tasks[index].completed = true;
-    console.log('Tarea completada.');
-    printTasks();
-  } else {
-    console.log('Índice de tarea inválido.');
+// Función para completar una tarea
+function completeTask() {
+  // Solicitamos al usuario el ID de la tarea a completar
+  const id = parseInt(readline.question('ID de la tarea a completar: '));
+
+  // Buscamos la tarea en la lista
+  const task = tasks.find(t => t.id === id);
+
+  // Completamos la tarea
+  if (task) {
+    task.completed = true;
   }
 }
 
-function handleInput(input) {
-  const parts = input.split(' ');
-  const command = parts[0];
+// Función principal
+function main() {
+  // Mostramos el menú de opciones
+  console.log('Menú de opciones');
+  console.log('1. Añadir tarea');
+  console.log('2. Eliminar tarea');
+  console.log('3. Completar tarea');
+  console.log('4. Salir');
 
-  switch (command) {
-    case 'add':
-      addTask(parts.slice(1).join(' '));
+  // Solicitamos al usuario la opción a ejecutar
+  const option = parseInt(readline.question('Opción: '));
+
+  // Ejecutamos la función correspondiente
+  switch (option) {
+    case 1:
+      addTask();
       break;
-    case 'remove':
-      removeTask(parseInt(parts[1]) - 1);
+    case 2:
+      deleteTask();
       break;
-    case 'complete':
-      completeTask(parseInt(parts[1]) - 1);
+    case 3:
+      completeTask();
       break;
-    case 'exit':
-      rl.close();
-      break;
+    case 4:
+      return;
     default:
-      console.log('Comando no válido.');
+      console.log('Opción no válida');
   }
+
+  // Mostramos la lista de tareas
+  console.log('Lista de tareas');
+  tasks.forEach(t => console.log(`ID: ${t.id}, Indicador: ${t.indicator}, Descripción: ${t.description}, Completado: ${t.completed}`));
 }
 
-function start() {
-  rl.question('Bienvenido a la gestión de tareas. Ingresa un comando (add / remove / complete / exit): ', (answer) => {
-    handleInput(answer);
-    if (answer !== 'exit') {
-      start();
-    }
-  });
-}
-
-start();
+// Ejecutamos la función principal
+main();
